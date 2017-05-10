374edb93fa41f015d793ee11585c343ae312bf00
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2017-05-10 15:33:12 +0900

    added CNN2 with 10 kernels

--- model.py
+++ model.py
@@ -16,6 +16,25 @@ class CNN(Model):
         x = self.fwd(x, train)
         return F.softmax_cross_entropy(x, t), F.accuracy(x, t)
 
+    def fwd(self, x, train):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.fc(x)
+        return x
+
+
+class CNN2(Model):
+
+    def __init__(self):
+        super(CNN2, self).__init__(
+            conv1=L.Convolution2D(1, 10, ksize=4, stride=1),
+            conv2=L.Convolution2D(10, 1, ksize=4, stride=1),
+            fc=L.Linear(None, 10)
+        )
+
+    def __call__(self, x, t, train=True):
+        x = self.fwd(x, train)
+        return F.softmax_cross_entropy(x, t), F.accuracy(x, t)
 
     def fwd(self, x, train):
         x = F.relu(self.conv1(x))
